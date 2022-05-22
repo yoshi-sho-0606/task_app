f3cf4825cb9a164bc571159ef5c42c908a0a6938
yoshi-sho-0606 <shoyoshioka23060202@icloud.com>
2022-05-22 17:01:00 +0900

    モーダルにcssを当てた

--- app/javascript/pages/task/components/TaskCreateModal.vue
+++ app/javascript/pages/task/components/TaskCreateModal.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    <button @click="handleCloseModal">×</button>
-    <p>createModal</p>
-    <label for="name">タスク名</label>
-    <input type="text" id="name" v-model="task.name">
-    <label for="body">詳細</label>
-    <textarea id="body" rows="5" v-model="task.body"></textarea>
-    <select id="status" v-model="task.status" class="form-control">
-      <option value="todo">TODO</option>
-      <option value="doing">DOING</option>
-    </select>
-    <button @click="handleCreateTask">新規登録</button>
+  <div id="create-modal">
+    <button class="close-btn" @click="handleCloseModal">閉じる</button>
+    <p class="modal-title">タスク 新規作成</p>
+    <form>
+      <div class="task-element">
+        <label for="name">タスク名</label>
+        <input type="text" id="name" v-model="task.name">
+      </div>
+      <div class="task-element">
+        <label for="body">詳細</label>
+        <textarea id="body" rows="5" v-model="task.body"></textarea>
+      </div>
+      <div class="task-element">
+        <label for="status">ステータス</label>
+        <select id="status" v-model="task.status" class="form-control">
+          <option value="todo">TODO</option>
+          <option value="doing">DOING</option>
+        </select>
+      </div>
+      <button class="submit-btn" @click="handleCreateTask">新規登録</button>
+    </form>
   </div>
 </template>
 
@@ -37,3 +46,34 @@ export default {
   }
 }
 </script>
+<style scoped>
+#create-modal {
+  height: 390px;
+  width: 250px;
+  margin: 0 auto;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 10px;
+}
+.modal-title {
+  border-bottom: 1px solid black;
+}
+label{
+  display: block;
+}
+.task-element {
+  padding-bottom: 10px;
+  border-bottom: 1px solid black;
+}
+.submit-btn {
+  margin-top: 10px;
+  margin-left: 150px;
+}
+button {
+    margin-bottom: 10px;
+    background-color: rgb(41, 161, 221);
+    width: 100px;
+    color: white;
+    border-radius: 100vh;
+  }
+</style>
--- app/javascript/pages/task/components/TaskEditModal.vue
+++ app/javascript/pages/task/components/TaskEditModal.vue
@@ -1,18 +1,24 @@
 <template>
-  <div>
-    <button @click="handleCloseModal">×</button>
-    <p>editModal</p>
-    <label for='name'>タスク名</label>
-    <input type='text' id='name' v-model="task.name">
-    <label for='body'>詳細</label>
-    <textarea id='body' rows='5' v-model="task.body"></textarea>
-    <label for='status'>ステータス</label>
-    <select id='status' v-model="task.status">
-      <option value='todo'>TODO</option>
-      <option value='doing'>DOING</option>
-      <option value='done'>DONE</option>
-    </select>
-    <button @click="handleUpdateTask(task)">更新</button>
+  <div id="edit-modal">
+    <button @click="handleCloseModal">閉じる</button>
+    <p class="modal-title">editModal</p>
+    <div class="task-element">
+      <label for='name'>タスク名</label>
+      <input type='text' id='name' v-model="task.name">
+    </div>
+    <div class="task-element">
+      <label for='body'>詳細</label>
+      <textarea id='body' rows='5' v-model="task.body"></textarea>
+    </div>
+    <div class="task-element">
+      <label for='status'>ステータス</label>
+      <select id='status' v-model="task.status">
+        <option value='todo'>TODO</option>
+        <option value='doing'>DOING</option>
+        <option value='done'>DONE</option>
+      </select>
+    </div>
+    <button class="submit-btn" @click="handleUpdateTask(task)">更新</button>
   </div>
 </template>
 
@@ -41,3 +47,35 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+#edit-modal {
+  height: 390px;
+  width: 250px;
+  margin: 0 auto;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 10px;
+}
+.modal-title {
+  border-bottom: 1px solid black;
+}
+label{
+  display: block;
+}
+.task-element {
+  padding-bottom: 10px;
+  border-bottom: 1px solid black;
+}
+.submit-btn {
+  margin-top: 10px;
+  margin-left: 150px;
+}
+button {
+    background-color: rgb(41, 161, 221);
+    width: 100px;
+    color: white;
+    border-radius: 100vh;
+  }
+</style>
+
